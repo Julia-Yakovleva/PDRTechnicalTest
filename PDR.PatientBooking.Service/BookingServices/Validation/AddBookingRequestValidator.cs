fbbc06a97dc119065e64db68f3df639bd30e4d7e
Julia Yakovleva <julia.yakovleva@dataart.com>
2020-10-14 20:02:09 +0300

    2: added booking service, moved booking creation to service, added booking validation mock

---
+++ PDR.PatientBooking.Service/BookingServices/Validation/AddBookingRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using PDR.PatientBooking.Service.BookingServices.Requests;
+using PDR.PatientBooking.Service.Validation;
+using System;
+
+namespace PDR.PatientBooking.Service.BookingServices.Validation
+{
+    public class AddBookingRequestValidator : IAddBookingRequestValidator
+    {
+        public PdrValidationResult ValidateRequest(AddBookingRequest request)
+        {
+            //validate for existing user
+            //validate for existing doctor
+            throw new NotImplementedException();
+        }
+    }
+}
